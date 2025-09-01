07be063c20bc58fbdfd5d22399ba59f73ad10799
phlegsx22 <phlegsxtrbigBoss@proton.me>
2025-09-01 14:24:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -100,6 +100,6 @@ app.post('/submit', async (req, res) => {
 
 
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
+app.listen(process.env.PORT_NO, () => {
+    console.log(`Server is running on port ${process.env.PORT_NO}`);
 })
